117ad73f59c71d255e21069ca73f0b7d6a8744ad
jongsungbae <90482015+jongsungbae@users.noreply.github.com>
2022-01-07 15:03:17 -0500

    javascript

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
@@ -11,4 +12,93 @@ document.addEventListener('scroll', () => {
     }else{
         navbar.classList.remove('add--dark');
     }
+    navbarMenu.classList.remove('open');
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (e)=>{
+    const target = e.target;
+    const link = target.dataset.link;
+    if(link == null){
+        return;
+    }
+    // console.log(e.target.dataset.link);
+    
+    scrollIntoView(link);
+})
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+    console.log('click');
+});
+
+// Handle click on "contact me" btn
+const homeContactBtn = document.querySelector('.home__btn');
+homeContactBtn.addEventListener('click', ()=>{
+    scrollIntoView('#contact');
+})
+
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// show 'arrow up' btn when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', ()=>{
+    if(window.scrollY > homeHeight / 2){
+        arrowUp.classList.add('visible');
+    }else{
+        arrowUp.classList.remove('visible');
+    }
+});
+
+// Handle click on the "arrow up" btn
+arrowUp.addEventListener('click', ()=>{
+    console.log('click');
+    scrollIntoView('#home');
+});
+
+
+// Project
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e)=>{
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    // console.log(filter);
+    if(filter == null){
+        return;
+    }
+
+    // REmove selection from the previous item and select the new one
+    const active = document.querySelector('.category__btn.selected');
+    active.classList.remove('selected');
+    const target = 
+        e.target.nodeName === 'BUTTON' ? e.target:e.target.parentNode;
+    target.classList.add('selected');
+
+    projectContainer.classList.add('ani-out');
+
+    setTimeout(()=>{
+        projects.forEach((project) => {
+            // console.log(project.dataset.type);
+            if(filter === '*' || filter === project.dataset.type){
+                project.classList.remove('invisible');
+            }else{
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('ani-out');
+    },300)
 });
